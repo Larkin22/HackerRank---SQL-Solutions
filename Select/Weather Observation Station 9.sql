ede8bbf21194ca52c527c69d2287edd4823f6977
Larkin22 <larkinjw@gmail.com>
2017-03-28 14:59:26 -0400

    Push SQL Solutions

---
+++ Select/Weather Observation Station 9.sql
@@ -0,0 +1,5 @@
+/*Query the list of CITY names from STATION that do not start with vowels.
+Your result cannot contain duplicates.*/
+
+SELECT DISTINCT CITY FROM STATION
+WHERE LEFT(city,1) NOT IN ('a', 'e', 'i', 'o', 'u')
