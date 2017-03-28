69634b829fc4c37bd2ec6c47837cc9cb4f7c355d
Larkin22 <larkinjw@gmail.com>
2017-03-28 12:59:08 -0400

    Push SQL Solutions

---
+++ Aggregation/Weather Observation Station 19.sql
@@ -0,0 +1,14 @@
+/*Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are
+the respective minimum and maximum values of Northern Latitude (LAT_N)
+and (c,d) are the respective minimum and maximum values of
+Western Longitude (LONG_W) in STATION.
+
+Query the Euclidean Distance between points P1 and P2
+and format your answer to display 4 decimal digits.*/
+
+/*SQL POWER() function returns the value of a number raised to another,
+where both of the numbers are passed as arguments.
+POWER(base, exponent)*/
+
+SELECT ROUND(SQRT(POWER(MAX(LAT_N)-MIN(LAT_N),2)+POWER(MAX(LONG_W)-MIN(LONG_W),2)),4)
+FROM STATION;
