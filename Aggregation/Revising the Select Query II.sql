78c65f6b9da46fd4fde673e5cb905fec16f91a3f
Larkin22 <larkinjw@gmail.com>
2017-03-28 03:10:23 -0400

    Push SQL Solutions

---
+++ Aggregation/Revising the Select Query II.sql
@@ -0,0 +1,4 @@
+/*Query the names of all American cities in CITY with populations
+larger than 120000. The CountryCode for America is USA.*/
+
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = "USA";
